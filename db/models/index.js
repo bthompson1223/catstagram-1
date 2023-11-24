22edb21e2dcb28d7a4c9effbdf733c71da0f709e
Bryan Thompson <114091741+bthompson1223@users.noreply.github.com>
2023-11-24 08:02:28 -0600

    testing

--- db/models/index.js
+++ db/models/index.js
@@ -11,7 +11,9 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(
+    "postgres://pokechat_user:6BBkjDoi2Kf1qjg3bSDPHgkNvZj4EfbM@dpg-clfrmcur45ec73btlgt0-a.oregon-postgres.render.com/pokechat"
+  );
 } else {
   sequelize = new Sequelize(
     config.database,
